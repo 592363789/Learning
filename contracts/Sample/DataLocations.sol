63d60afb135a79306d23f011da6a3cbf8fa72cb5
592363789 <592363789@qq.com>
2024-07-29 20:08:58 +0800

    done

---
+++ contracts/Sample/DataLocations.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract DataLocations {
+    uint256[] public arr;
+    mapping(uint256 => address) map;
+
+    struct MyStruct {
+        uint256 foo;
+    }
+
+    mapping(uint256 => MyStruct) myStructs;
+
+    function f() public {
+        // call _f with state variables
+        _f(arr, map, myStructs[0]);
+
+        // get a struct from a mapping
+        MyStruct storage myStruct = myStructs[1];
+        // create a struct in memory
+        MyStruct memory myMemStruct = MyStruct(0);
+    }
+
+    function _f(
+        uint256[] storage _arr,
+        mapping(uint256 => address) storage _map,
+        MyStruct storage _myStruct
+    ) internal view  {
+        address add = _map[2];
+        // do something with storage variables
+    }
+
+    // You can return memory variables
+    function g(uint256[] memory _arr) public pure returns (uint256[] memory) {
+        // do something with memory array
+        //_arr.pop();
+        _arr[0] = 1;
+        return _arr;
+    }
+
+    function h(uint256[] calldata _arr) external {
+        // do something with calldata array
+        //_arr.pop();
+        //_arr[0] = 1;
+        
+    }
+}
---
+++ contracts/Sample/ImportStruct.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./Struct.sol";
+
+contract Todos1 {
+    // An array of 'Todo' structs
+    Todos.Todo[] public todos;
+}
